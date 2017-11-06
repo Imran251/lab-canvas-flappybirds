f3e9ed3ebdd3d96ad157e3695ddd1619a969b2d4
Imran Syed <syed.imran2512@gmail.com>
2017-11-05 23:13:30 -0500

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -9,4 +9,12 @@ window.onload = function() {
     };
   }
 
+  create: function () {
+        // Change the background color of the game to match fullstackfest
+        game.add.tileSprite(0, 0, 800, 600, 'background');
+        this.labelBrand = game.add.text(230, 150, " ------------\n| PUSHY BIRD |\n ------------", { font: "60px VT323", fill: "#ffffff" });
+        this.labelStart = game.add.text(260, 400, "Press button to start", { font: "30px VT323", fill: "#ffffff" });
+        this.labelFooter = game.add.text(320, 550, "@rgalindo33 2016", { font: "20px VT323", fill: "#ffffff" });
+
 };
+}
